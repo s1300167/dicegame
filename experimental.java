7a30fa6bba9240c9b9ca29ab364d11de429ecc43
s1300167 <136040755+s1300167@users.noreply.github.com>
2023-06-14 03:34:43 +0900

    Greetings added

---
+++ experimental.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class experimental{
+  public ststic void main(String[] args){
+    System.out.println("What is your name?");
+    
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    
+    System.out.println("Hello, " + str + "!");
+  }
+}
